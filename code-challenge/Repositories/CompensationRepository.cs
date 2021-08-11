6e0e050832c46e467b7a0632f414c27ff727e2f2
Prathamesh Punde <32567231+WhiteBeast97@users.noreply.github.com>
2021-08-11 15:06:17 -0400

    Add files via upload

---
+++ code-challenge/Repositories/CompensationRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using challenge.Data;
+
+namespace challenge.Repositories
+{
+    public class CompensationRepository : ICompensationRepository
+    {
+        private readonly CompensationContext _compensationContext;
+        private readonly ILogger<ICompensationRepository> _logger;
+
+        public CompensationRepository(ILogger<ICompensationRepository> logger, CompensationContext compensationContext)
+        {
+            _compensationContext = compensationContext;
+            _logger = logger;
+        }
+
+        public Compensation createComp(Compensation compensation)
+        {
+            _compensationContext.Compensations.Add(compensation);
+            return compensation;
+        }
+
+        public Compensation getCompByEId(string employeeId)
+        {
+            return _compensationContext.Compensations.AsEnumerable().Where(comp => comp.EmployeeId ==employeeId).SingleOrDefault();
+        }
+
+        public Task SaveAsync()
+        {
+            return _compensationContext.SaveChangesAsync();
+        }
+
+        public Compensation Remove(Compensation compensation)
+        {
+            return _compensationContext.Remove(compensation).Entity;
+        }
+    }
+}
---
+++ code-challenge/Repositories/ICompensationRepository.cs
@@ -0,0 +1,13 @@
+﻿using challenge.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace challenge.Repositories
+{
+    public interface ICompensationRepository
+    {
+        Compensation getCompByEId(String employeeId);
+        Compensation createComp(Compensation compensation);
+        Task SaveAsync();
+    }
+}
