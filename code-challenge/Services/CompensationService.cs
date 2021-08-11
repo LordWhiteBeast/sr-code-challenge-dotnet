8902b50e9089654f1b85583b1c1edf43d71d52e8
Prathamesh Punde <32567231+WhiteBeast97@users.noreply.github.com>
2021-08-11 15:06:32 -0400

    Add files via upload

---
+++ code-challenge/Services/CompensationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class CompensationService : ICompensationService
+    {
+        private readonly ICompensationRepository _compensationRepository;
+        private readonly ILogger<CompensationService> _logger;
+
+        public CompensationService(ILogger<CompensationService> logger, ICompensationRepository compensationRepository)
+        {
+            _compensationRepository = compensationRepository;
+            _logger = logger;
+        }
+        //To Create a compensation
+        public Compensation Create(Compensation compensation)
+        {
+            if (compensation != null)
+            {
+                _compensationRepository.createComp(compensation);
+                _compensationRepository.SaveAsync().Wait();
+            }
+
+            return compensation;
+        }
+        // To read a compensation using employeeId
+        public Compensation GetById(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                return _compensationRepository.getCompByEId(id);
+            }
+
+            return null;
+        }
+
+    }
+}
